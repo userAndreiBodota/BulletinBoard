73cffc8bc85bcd9fabaf4f7442f5eece101a44c5
Andrei Bodota <bodotaandrei@gmail.com>
2023-04-29 11:49:55 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BulletinBoard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
